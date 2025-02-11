8c82d6d839e24a7a224294db333b9fc36b851c4a
Krakowiecki <Chriskrak3000@gmail.com>
2025-02-11 14:14:23 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,8 +1,13 @@
 g=-9.81
+    #Gravity. The gravity displayed is the gravity on earth
 k=5
+    #Initial Velocity
 h=1.8
+    #Hight
 start=-3
+    #Starting Time
 end=3
+    #Ending Time
 for x in range (start,end+1):
     y=-g*x**2+k*x+h
     y=round(y,2)
